80ca12024b08707955e11488b77e5696063097c2
sahal <sahalshalu830@gmail.com>
2023-09-25 17:25:34 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
 import RowPost from './components/RowPost/RowPost';
-import {originals,action} from './urls'
+import {originals, action, trending, ComedyMovies} from './urls'
 
 function App() {
   return (
@@ -11,6 +11,9 @@ function App() {
       <Banner/>
       <RowPost url={originals} title='Netflix Orginals'/>
       <RowPost  url={action} title='Action' isSmall />
+      <RowPost url={trending} title='Trending' isSmall/>
+     
+      
       
      
     </div>
--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,7 @@
 import {API_KEY}  from './constences/constences'
 export const originals = `discover/tv?api_key=${API_KEY}&with_networks=213`
 export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
+export const trending = `discover/movie?api_key=${API_KEY}&with_genres=28`
+//export const ComedyMovies = `discover/movie?api_key=${API_KEY}&with_genres=28`
+
+
